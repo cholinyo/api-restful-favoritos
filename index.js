60f658a69c96af547d889715f856057c48299a31
Vicente Caruncho Ramos <cholinyo@gmail.com>
2019-03-30 23:07:30 +0100

    initial index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var express = require ('express')
+var app = express();
+
+app.listen(3678, function(){
+    console.log('API REST FAVORITOS FUNCIONANDO en http://localhost:3678')
+});
+
+
